97d2573ed740edd9539b96fd6786ca6b69c46b6a
Mauricio Cruz <mawcp7@gmail.com>
2024-05-15 12:40:59 -0600

    chore: Update server start message

--- src/index.ts
+++ src/index.ts
@@ -16,18 +16,16 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 const PORT = process.env.PORT || 3001;
-const API_PREFIX = process.env.API_PREFIX || "/api/v1";
 
 app.options("*", cors())
 app.use(cors())
 
-app.use(`${API_PREFIX}/orders`, orderRouter);
-// app.use(`${API_PREFIX}`, router);
+app.use("/", orderRouter);
 
 async function startServer() {
     await syncConnection();
     app.listen(PORT, () => {
-        logger.success(`Server running on http://localhost:${PORT}${API_PREFIX}`);
+        logger.success(`Server running on http://localhost:${PORT}$`);
     });
 }
 
